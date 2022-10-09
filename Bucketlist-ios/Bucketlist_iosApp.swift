b2fe723369acc61f697e834dbc4d2ff60b9d502a
Safal Neupane <safal.npane@gmail.com>
2022-10-09 19:35:23 +0545

    Initial commit

---
+++ Bucketlist-ios/Bucketlist_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucketlist_iosApp.swift
+//  Bucketlist-ios
+//
+//  Created by Safal Neupane on 9/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bucketlist_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
